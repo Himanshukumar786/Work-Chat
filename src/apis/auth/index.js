77dd159d82ab776a0ca5a869834eecdad17cb8fc
Himanshukumar786 <himanshu.kumar0104@gmail.com>
2025-03-26 22:38:08 +0530

    Added apis and react query hooks

---
+++ src/apis/auth/index.js
@@ -0,0 +1,31 @@
+import axios from '@/config/axiosConfig';
+
+export const signUpRequest = async ({ email, password, username }) => {
+    try {
+        const response = await axios.post('/uses/signup', {
+            email,
+            password,
+            username
+        });
+        return response.data;
+
+    } catch (error) {
+        console.log(error);
+        throw error.response.data;
+    }
+};
+
+export const signInRequest = async ({ email, password }) => {
+    try {
+        const response = await axios.post('/uses/signin', {
+            email,
+            password,
+        });
+        return response.data;
+
+    } catch (error) {
+        console.log(error);
+        throw error.response.data;
+    }
+};
+
